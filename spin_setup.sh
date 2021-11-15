97c77f3738bb5e9c26cd0a5301e797aecccb45e7
Hugo Vacher <hugo.vacher@shopify.com>
2021-11-15 17:29:45 -0500

    Automatically install dev-test-runner

--- spin_setup.sh
+++ spin_setup.sh
@@ -1,2 +1,7 @@
 #!/bin/zsh
+set -e
+
 git config --global user.email "$SPIN_USER_EMAIL"
+
+extension=$(while read file; do echo $(git log --pretty=format:%ad -n 1 --date=raw -- $file) $file; done < <(ls dev-test-runner/*.vsix) | sort -k1,1n | tail -n 1 | cut -d " " -f3)
+code --install-extension $extension
