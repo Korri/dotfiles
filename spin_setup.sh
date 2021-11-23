f04732fc860fd68e12d22bdf404680224a7ea4f9
Hugo Vacher <hugo.vacher@shopify.com>
2021-11-23 18:15:20 -0500

    Hard code email as env is gone

--- spin_setup.sh
+++ spin_setup.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 set -e
 
-git config --global user.email "$SPIN_USER_EMAIL"
+echo '[user]
+	email = hugo.vacher@shopify.com
+' >> ~/.gitconfig_local
 
 git submodule update --init --recursive dev-test-runner
 extension=$(while read file; do echo $(git log --pretty=format:%ad -n 1 --date=raw -- $file) $file; done < <(ls dev-test-runner/*.vsix) | sort -k1,1n | tail -n 1 | cut -d " " -f3)
