d234a4f2ce95619f6e21441551af253e13119750
Hugo Vacher <hugo.vacher@shopify.com>
2021-11-23 09:51:35 -0500

    Checkout submodule

--- spin_setup.sh
+++ spin_setup.sh
@@ -3,5 +3,6 @@ set -e
 
 git config --global user.email "$SPIN_USER_EMAIL"
 
+git submodule update --init --recursive dev-test-runner
 extension=$(while read file; do echo $(git log --pretty=format:%ad -n 1 --date=raw -- $file) $file; done < <(ls dev-test-runner/*.vsix) | sort -k1,1n | tail -n 1 | cut -d " " -f3)
 code --install-extension $extension
