587f90f17a98a53d758c31302bdd5e3dd2865d43
Hugo Vacher <korrigan33@gmail.com>
2022-02-13 10:55:06 -0600

    Remove dev-test-runner

--- spin_setup.sh
+++ spin_setup.sh
@@ -4,7 +4,3 @@ set -xe
 echo '[user]
 	email = hugo.vacher@shopify.com
 ' >> ~/.gitconfig_local
-
-git submodule update --init --recursive dev-test-runner
-extension=$(while read file; do echo $(git log --pretty=format:%ad -n 1 --date=raw -- $file) $file; done < <(ls dev-test-runner/*.vsix) | sort -k1,1n | tail -n 1 | cut -d " " -f3)
-code --install-extension $extension
